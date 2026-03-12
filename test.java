8b78781a931b60bd603b8500fc48d62edbd2392a
jaypark0327 <168830366+jaypark0327@users.noreply.github.com>
2026-03-12 14:31:16 +0900

    Update test.java
    Add import

--- test.java
+++ test.java
@@ -1,3 +1,13 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import android.util.Slog; // 이미 있을 확률이 높지만 확인 필요
+
 private float getLearnedTimeout(String packageName) {
     if (mModelInterpreter == null) return 30.0f;
 
