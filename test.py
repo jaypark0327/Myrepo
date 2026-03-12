17a2a658420f1fa6f8c20f1a9d87a0aa1d1de786
jaypark0327 <168830366+jaypark0327@users.noreply.github.com>
2026-03-12 13:37:11 +0900

    Update test.py
    add save logic

--- test.py
+++ test.py
@@ -1,30 +1,26 @@
 import tensorflow as tf
 import os
 
-# 1. 가중치 변수 정의 (전역 변수로 설정)
 bucket_size = 10000
-embedding = tf.Variable(tf.random.uniform([bucket_size, 8], -0.1, 0.1), name='embedding')
-w1 = tf.Variable(tf.random.normal([9, 1], stddev=0.1), name='w1')
-b1 = tf.Variable(tf.zeros([1]), name='b1')
-learning_rate = tf.constant(0.01)
 
-# 2. 내보낼 모듈 정의
 class PowerModelModule(tf.Module):
     def __init__(self):
         super().__init__()
-        self.v = [embedding, w1, b1, learning_rate] # 변수 추적용
+        # 초기 Bias를 15.0으로 설정
+        self.embedding = tf.Variable(tf.random.uniform([bucket_size, 8], -0.1, 0.1), name='embedding')
+        self.w1 = tf.Variable(tf.random.normal([9, 1], stddev=0.1), name='w1')
+        self.b1 = tf.Variable(tf.constant([15.0], dtype=tf.float32), name='b1')
+        self.learning_rate = tf.constant(0.01)
 
     @tf.function(input_signature=[
         tf.TensorSpec(shape=[1], dtype=tf.int32, name='app_id'),
         tf.TensorSpec(shape=[1], dtype=tf.float32, name='usage_time')
     ])
     def predict(self, app_id, usage_time):
-        app_vec = tf.gather(embedding, app_id)
+        app_vec = tf.gather(self.embedding, app_id)
         app_vec = tf.reshape(app_vec, [1, 8])
-        usage_time_vec = tf.reshape(usage_time, [1, 1])
-        
-        x = tf.concat([app_vec, usage_time_vec], axis=1)
-        prediction = tf.matmul(x, w1) + b1
+        x = tf.concat([app_vec, tf.reshape(usage_time, [1, 1])], axis=1)
+        prediction = tf.matmul(x, self.w1) + self.b1
         return {'output': prediction}
 
     @tf.function(input_signature=[
@@ -33,46 +29,57 @@ class PowerModelModule(tf.Module):
         tf.TensorSpec(shape=[1, 1], dtype=tf.float32, name='label')
     ])
     def train(self, app_id, usage_time, label):
-        app_vec = tf.gather(embedding, app_id)
+        app_vec = tf.gather(self.embedding, app_id)
         app_vec = tf.reshape(app_vec, [1, 8])
-        usage_time_vec = tf.reshape(usage_time, [1, 1])
-        
-        x = tf.concat([app_vec, usage_time_vec], axis=1)
-        pred = tf.matmul(x, w1) + b1
+        x = tf.concat([app_vec, tf.reshape(usage_time, [1, 1])], axis=1)
+        pred = tf.matmul(x, self.w1) + self.b1
         diff = pred - label
         
-        # SGD 업데이트
         grad_w = tf.matmul(tf.transpose(x), diff)
-        w1.assign_sub(learning_rate * grad_w)
-        b1.assign_sub(learning_rate * tf.reshape(diff, [1]))
+        self.w1.assign_sub(self.learning_rate * grad_w)
+        self.b1.assign_sub(self.learning_rate * tf.reshape(diff, [1]))
         
-        grad_emb = tf.reshape(diff * w1[:8, 0], [1, 8])
-        updated_emb = app_vec - learning_rate * grad_emb
-        
-        embedding.assign(tf.tensor_scatter_nd_update(
-            embedding, tf.reshape(app_id, [1, 1]), updated_emb))
+        grad_emb = tf.reshape(diff * self.w1[:8, 0], [1, 8])
+        self.embedding.assign(tf.tensor_scatter_nd_update(
+            self.embedding, tf.reshape(app_id, [1, 1]), app_vec - self.learning_rate * grad_emb))
             
         return {'loss': tf.square(diff)}
 
-# 3. SavedModel로 임시 저장
+    # --- 추가: 현재 가중치를 추출하는 서명 ---
+    @tf.function(input_signature=[])
+    def export_weights(self):
+        return {
+            'emb_out': self.embedding,
+            'w1_out': self.w1,
+            'b1_out': self.b1
+        }
+
+    # --- 추가: 가중치를 주입하는 서명 ---
+    @tf.function(input_signature=[
+        tf.TensorSpec(shape=[bucket_size, 8], dtype=tf.float32, name='emb_in'),
+        tf.TensorSpec(shape=[9, 1], dtype=tf.float32, name='w1_in'),
+        tf.TensorSpec(shape=[1], dtype=tf.float32, name='b1_in')
+    ])
+    def import_weights(self, emb_in, w1_in, b1_in):
+        self.embedding.assign(emb_in)
+        self.w1.assign(w1_in)
+        self.b1.assign(b1_in)
+        return {'status': tf.constant(1)}
+
+# 저장 및 변환
 model_module = PowerModelModule()
 SAVED_MODEL_DIR = "temp_power_model"
-tf.saved_model.save(
-    model_module, 
-    SAVED_MODEL_DIR, 
-    signatures={
-        'predict': model_module.predict,
-        'train': model_module.train
-    }
-)
+tf.saved_model.save(model_module, SAVED_MODEL_DIR, 
+                    signatures={
+                        'predict': model_module.predict, 
+                        'train': model_module.train,
+                        'export': model_module.export_weights,
+                        'import': model_module.import_weights
+                    })
 
-# 4. TFLite 변환 (SavedModel로부터 로드)
 converter = tf.lite.TFLiteConverter.from_saved_model(SAVED_MODEL_DIR)
 converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS]
 tflite_model = converter.convert()
 
-# 5. 최종 tflite 파일 저장
 with open('power_manager_model.tflite', 'wb') as f:
     f.write(tflite_model)
-
-print("성공: power_manager_model.tflite 파일이 생성되었습니다.")
