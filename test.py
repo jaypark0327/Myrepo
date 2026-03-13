b9f5abedae1a1bebe748919f21c0ca40c47274db
jaypark0327 <168830366+jaypark0327@users.noreply.github.com>
2026-03-13 14:54:39 +0900

    Update test.py
    val err

--- test.py
+++ test.py
@@ -6,16 +6,16 @@ class PowerModelModule(tf.Module):
         super().__init__()
         bucket_size = 10000
         # 1. 가중치 초기화
-        # Embedding: 앱별 고유 특성 (-0.1 ~ 0.1 사이로 안정적 시작)
+        # Embedding: [10000, 8] 차원
         self.embedding = tf.Variable(
             tf.random.uniform([bucket_size, 8], -0.1, 0.1), name='embedding')
-        # W1: 입력값들과 결과값을 연결 (9차원: 임베딩 8 + 사용시간 1)
+        # W1: [9, 1] 차원 (8개 임베딩 + 1개 사용시간)
         self.w1 = tf.Variable(
             tf.random.normal([9, 1], stddev=0.1), name='w1')
         # B1: 공통 Bias (15초 고정)
         self.b1 = tf.Variable(tf.constant([15.0], dtype=tf.float32), name='b1')
         
-        # 학습률 설정 (안정적인 0.01 ~ 0.05 권장)
+        # 학습률 설정 (안정적인 0.025)
         self.learning_rate = tf.constant(0.025)
 
     @tf.function(input_signature=[
@@ -25,19 +25,19 @@ class PowerModelModule(tf.Module):
     ])
     def train(self, app_id, usage_time, label):
         with tf.GradientTape() as tape:
-            # 1. 앱별 임베딩 추출 및 차원 확장 [1, 8]
+            # 1. 앱별 임베딩 추출 [1, 8]
             app_vec = tf.gather(self.embedding, app_id)
             app_vec_expanded = tf.expand_dims(app_vec, 0)
             
-            # 2. 특징 결합 (usage_time을 [1, 1]로 확장하여 결합)
+            # 2. 특징 결합 [1, 9]
             usage_time_expanded = tf.expand_dims(usage_time, 0)
             inputs = tf.concat([app_vec_expanded, usage_time_expanded], axis=1)
 
-            # 3. 예측값 계산 (y = Wx + b)
+            # 3. 예측값 계산
             pred = tf.matmul(inputs, self.w1) + self.b1
             pred_final = tf.reshape(pred, [1])
 
-            # 4. Loss 계산 (Huber Loss: 오차가 클 때 기울기 폭발 방지)
+            # 4. Huber Loss (오차 폭주 및 NaN 방지)
             diff = pred_final - label
             loss = tf.where(
                 tf.abs(diff) <= 1.0,
@@ -45,25 +45,29 @@ class PowerModelModule(tf.Module):
                 tf.abs(diff) - 0.5
             )
 
-        # 5. 가중치 업데이트 (b1은 제외하여 15초 고정)
+        # 5. 가중치 업데이트 (b1 제외)
         trainable_vars = [self.w1, self.embedding]
         grads = tape.gradient(loss, trainable_vars)
 
-        # 6. Gradient Clipping (-1.0 ~ 1.0)
+        # 6. Gradient Clipping
         clipped_grads = [tf.clip_by_value(g, -1.0, 1.0) for g in grads]
 
-        # W1 업데이트
+        # --- 업데이트 로직 (ValueError 방지 핵심) ---
+        
+        # W1 업데이트: 가중치 변수 업데이트
         self.w1.assign_sub(self.learning_rate * clipped_grads[0])
         
-        # Embedding 업데이트 (특정 앱 행만 교체)
-        # indices 구조를 [1, 1]로 명확히 하여 ValueError 방지
+        # Embedding 업데이트: scatter_nd_update 사용
+        # 인덱스를 [[app_id]] 형태의 [1, 1] 구조로 생성
         indices = tf.reshape(app_id, [1, 1])
+        
+        # 해당 앱의 업데이트될 새 벡터 계산 [1, 8]
         grad_emb_specific = tf.gather(clipped_grads[1], app_id)
         new_vec = app_vec - (self.learning_rate * grad_emb_specific)
-        new_vec_expanded = tf.expand_dims(new_vec, 0)
+        new_vec_reshaped = tf.reshape(new_vec, [1, 8])
 
-        self.embedding.assign(tf.tensor_scatter_nd_update(
-            self.embedding, indices, new_vec_expanded))
+        # 변수 자체의 메서드를 호출하여 특정 행만 교체
+        self.embedding.scatter_nd_update(indices, new_vec_reshaped)
 
         return {'loss': tf.reduce_mean(loss), 'pred': pred_final}
 
@@ -82,45 +86,4 @@ class PowerModelModule(tf.Module):
 
     @tf.function(input_signature=[
         tf.TensorSpec(shape=[10000, 8], dtype=tf.float32, name='emb_in'),
-        tf.TensorSpec(shape=[9, 1], dtype=tf.float32, name='w1_in'),
-        tf.TensorSpec(shape=[1], dtype=tf.float32, name='b1_in')
-    ])
-    def import_weights(self, emb_in, w1_in, b1_in):
-        self.embedding.assign(emb_in)
-        self.w1.assign(w1_in)
-        self.b1.assign(b1_in)
-        return {'status': tf.constant([1.0], dtype=tf.float32)}
-
-    @tf.function(input_signature=[
-        tf.TensorSpec(shape=[1], dtype=tf.float32, name='dummy_in')
-    ])
-    def export_weights(self, dummy_in):
-        # 최적화 방지를 위해 dummy_in을 연산에 포함
-        fake_op = dummy_in * 0.0
-        return {
-            'emb_out': tf.identity(self.embedding),
-            'w1_out': tf.identity(self.w1),
-            'b1_out': tf.identity(self.b1) + fake_op
-        }
-
-# --- TFLite 변환 로직 ---
-model = PowerModelModule()
-export_dir = 'power_manager_saved_model'
-tf.saved_model.save(model, export_dir, signatures={
-    'train': model.train,
-    'predict': model.predict,
-    'import': model.import_weights,
-    'export': model.export_weights
-})
-
-converter = tf.lite.TFLiteConverter.from_saved_model(export_dir)
-converter.target_spec.supported_ops = [
-    tf.lite.OpsSet.TFLITE_BUILTINS,
-    tf.lite.OpsSet.SELECT_TF_OPS
-]
-tflite_model = converter.convert()
-
-with open('power_manager_model.tflite', 'wb') as f:
-    f.write(tflite_model)
-
-print("TFLite Model Build Success!")
+        tf.TensorSpec(shape=[9,
