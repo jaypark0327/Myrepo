97970e31c7aa095f946249fb9e66fad916d46785
jaypark0327 <168830366+jaypark0327@users.noreply.github.com>
2026-03-13 15:36:25 +0900

    Update test.py
    fit too slow train

--- test.py
+++ test.py
@@ -15,55 +15,51 @@ class PowerModelModule(tf.Module):
         # 학습률 (안정적인 수치)
         self.learning_rate = tf.constant(0.025)
 
-    @tf.function(input_signature=[
+     @tf.function(input_signature=[
         tf.TensorSpec(shape=[], dtype=tf.int32, name='app_id'),
         tf.TensorSpec(shape=[1], dtype=tf.float32, name='usage_time'),
         tf.TensorSpec(shape=[1], dtype=tf.float32, name='label')
     ])
     def train(self, app_id, usage_time, label):
         with tf.GradientTape() as tape:
-            # 1. 앱별 임베딩 추출 [1, 8]
             app_vec = tf.gather(self.embedding, app_id)
             app_vec_expanded = tf.expand_dims(app_vec, 0)
             
-            # 2. 특징 결합 [1, 9]
-            usage_time_expanded = tf.expand_dims(usage_time, 0)
-            inputs = tf.concat([app_vec_expanded, usage_time_expanded], axis=1)
+            # 입력값 스케일링 (큰 값 들어올 경우 대비 안전장치)
+            usage_time_scaled = tf.expand_dims(usage_time, 0) * 0.1
+            inputs = tf.concat([app_vec_expanded, usage_time_scaled], axis=1)
 
-            # 3. 예측값 계산
             pred = tf.matmul(inputs, self.w1) + self.b1
             pred_final = tf.reshape(pred, [1])
 
-            # 4. Huber Loss (안정적인 오차 계산)
-            diff = pred_final - label
-            loss_val = tf.where(
-                tf.abs(diff) <= 1.0,
-                0.5 * tf.square(diff),
-                tf.abs(diff) - 0.5
-            )
-            loss_mean = tf.reduce_mean(loss_val)
+            # 1. 다시 MSE(Square)로 복귀하여 학습 속도 향상
+            loss_mean = tf.reduce_mean(tf.square(pred_final - label))
 
-        # 5. 가중치 업데이트
-        grads = tape.gradient(loss_mean, [self.w1, self.embedding])
-        clipped_grads = [tf.clip_by_value(g, -1.0, 1.0) for g in grads]
+        trainable_vars = [self.w1, self.embedding]
+        grads = tape.gradient(loss_mean, trainable_vars)
 
-        # W1 업데이트
+        # 2. 범위를 -50.0 ~ 50.0으로 대폭 확장 (기존 1.0은 너무 작았음)
+        # 또는 tf.clip_by_global_norm을 써서 방향성을 보존
+        clipped_grads, _ = tf.clip_by_global_norm(grads, 50.0)
+
+        # 3. 업데이트 적용 (학습률 0.1 추천)
         self.w1.assign_sub(self.learning_rate * clipped_grads[0])
         
-        # Embedding 업데이트 (TFLite 호환 방식으로 우회)
         indices = tf.reshape(app_id, [1, 1])
         specific_grad = tf.gather(clipped_grads[1], app_id)
         new_vec = app_vec - (self.learning_rate * specific_grad)
+        
+        # 가중치 값 자체가 무한대로 가는 것을 방지 (NaN 최종 수비)
+        new_vec = tf.clip_by_value(new_vec, -100.0, 100.0)
         new_vec_reshaped = tf.reshape(new_vec, [1, 8])
 
-        updated_emb = tf.tensor_scatter_nd_update(self.embedding, indices, new_vec_reshaped)
-        self.embedding.assign(updated_emb)
+        self.embedding.scatter_nd_update(indices, new_vec_reshaped)
 
-        # Java의 float[1][1]과 맞추기 위해 [1, 1]로 리셰이프
         return {
             'loss': tf.reshape(loss_mean, [1, 1]),
             'pred': tf.reshape(pred_final, [1, 1])
         }
+
 
     @tf.function(input_signature=[
         tf.TensorSpec(shape=[], dtype=tf.int32, name='app_id'),
